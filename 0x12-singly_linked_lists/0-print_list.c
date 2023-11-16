ed237101cfbee037facc737592b99b7e26302933
72730882 <faizamohammed72730882@gmail.com>
2023-11-16 04:56:43 -0800

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,27 @@
-#ifndef _LINKED_LIST
-#define _LINKED_LIST
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for alx project
- */
-typedef struct list_s
+  * print_list - Prints all elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of nodes
+  */
+size_t print_list(const list_t *h)
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	size_t count = 0;
 
-int _strlen(const char *s);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-#endif
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
