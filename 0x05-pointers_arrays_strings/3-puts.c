9743ed1a1e9d3df5274821c13d0309cee8190f52
72730882 <faizamohammed72730882@gmail.com>
2023-11-17 10:33:25 -0800

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * _puts - Print a string
+ * _puts - Prints a string
  * @str: The string to print
  * Return: void
  */
 
 void _puts(char *str)
+
 {
-	for (; *str != '0'; str++)
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str);
+	_putchar(*str);
 	}
 	_putchar('\n');
 }
